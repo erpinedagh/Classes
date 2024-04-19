3ede25d88ed8dc77acfd0eb293fff543732c3cca
erpinedagh <146993987+erpinedagh@users.noreply.github.com>
2024-04-19 09:44:28 -0700

    name change

--- MAE 155B/A2/Peerformance.m
+++
@@ -1,9 +0,0 @@
-% Performance Section
-% Last Update: 4/16
-
-%% Takeoff Analysis 
-CL_TO = CL_Max * 0.8;% Takeoff Lift Coefficent
-V_TO =  sqrt(2*Wo/CL_TO/rho/S); % Takeoff Speed
-a_mean = ;
-S_g = (V_TO^2)/2/a_mean;
-
